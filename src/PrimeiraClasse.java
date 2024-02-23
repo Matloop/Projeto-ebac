dc77d509979bf037a0574615f7243d6908022ff4
matloop <matheusdiasestacioo@gmail.com>
2024-02-23 17:08:03 -0300

    src/

---
+++ src/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+public class PrimeiraClasse {
+    public static void main (String args[]) {
+        System.out.println("Oi");
+        Cliente cliente = new Cliente();
+        cliente.setEndereco("penis 12");
+        System.out.println(cliente.getEndereco());
+
+        cliente.
+    }
+}
